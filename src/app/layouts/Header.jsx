d1b9131af0db7470a105ae53b86f4a34e27f6059
PerpetuumG <perpetuumg@gmail.com>
2024-07-03 22:07:56 +0300

    Header

--- src/app/layouts/Header.jsx
+++ src/app/layouts/Header.jsx
@@ -4,10 +4,15 @@ import Container from '@/app/layouts/Container';
 import Link from 'next/link';
 import { MENU, FLAGS } from '@/lib/constants';
 import Image from 'next/image';
+import { FaBars } from 'react-icons/fa';
+import { IoMdClose } from 'react-icons/io';
+import { useState } from 'react';
 
 const Header = () => {
+  const [showHideMenu, setShowHideMenu] = useState(false);
+
   return (
-    <header className={'min-h-[60px] md:leading-[60px] z-10 relative bg-slate-300'}>
+    <header className={'min-h-[60px] md:leading-[60px] z-10 relative'}>
       <Container>
         <div className={'md:flex justify-between items-center gap-5 w-full'}>
           {/*LOGO*/}
@@ -15,17 +20,29 @@ const Header = () => {
             <Link href={'/'} className={'text-2xl font-bold text-sky-600'}>
               ScorIt
             </Link>
+
+            {showHideMenu ? (
+              <IoMdClose
+                className={'text-xl cursor-pointer md:hidden'}
+                onClick={() => setShowHideMenu(!showHideMenu)}
+              />
+            ) : (
+              <FaBars
+                className={'text-xl cursor-pointer md:hidden'}
+                onClick={() => setShowHideMenu(!showHideMenu)}
+              />
+            )}
           </div>
 
           {/*MENU*/}
-          <nav>
+          <nav className={showHideMenu ? 'md:block py-3' : 'hidden md:block'}>
             <ul className={'md:flex gap-2 py-4 md:py-0'}>
               {/*MENU LINKS*/}
               {MENU.map((item, index) => (
                 <li key={index}>
                   <Link
                     href={item.link}
-                    className={'px-0 md:px-2 md:py-0 py-2 leading-[1rem] md:leading-none'}
+                    className={'px-0 md:px-2 md:py-0 py-2 md:text-white md:hover:text-slate-100 duration-100 leading-[1rem] md:leading-none'}
                   >
                     {item.title}
                   </Link>
@@ -45,7 +62,7 @@ const Header = () => {
               </li>
 
               {/*LANGUAGES*/}
-              <div className={'flex justify-end items-center gap-2'}>
+              <div className={'flex md:justify-end items-center gap-2 mt-5 md:mt-0'}>
                 {FLAGS.map((item, index) => (
                   <Image
                     key={index}
